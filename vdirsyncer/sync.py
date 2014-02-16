3af84ccf903c7710174544d661be75b717e7411e
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-02-16 15:37:32 +0100

    Isolate namespaces a bit in sync function
    Fix some bugs

--- vdirsyncer/sync.py
+++ vdirsyncer/sync.py
@@ -13,6 +13,7 @@
     :license: MIT, see LICENSE for more details.
 '''
 
+import itertools
 
 def sync(storage_a, storage_b, status):
     '''Syncronizes two storages.
@@ -27,58 +28,73 @@ def sync(storage_a, storage_b, status):
         at the next sync. If this is the first sync, an empty dictionary should
         be provided.
     '''
-    list_a = dict(storage_a.list_items())
-    list_b = dict(storage_b.list_items())
+    list_a = dict(storage_a.list())  # {uid: etag}
+    list_b = dict(storage_b.list())
 
-    prefetch_items_from_a = []
-    prefetch_items_from_b = []
-    actions = []  # list(tuple(action, uid, source, dest))
+    items_a = {}  # items prefetched for copy
+    items_b = {}  # {uid: (item, etag)}
 
-    for uid in set(list_a).union(set(list_b)):
-        if uid not in status:
-            if uid in list_a and uid in list_b:  # missing status
-                status[uid] = (list_a[uid], list_b[uid])  # TODO: might need some kind of diffing too?
-            elif uid in list_a and uid not in list_b:  # new item in a
-                prefetch_items_from_a.append(uid)
-                actions.append(('upload', uid, 'a', 'b'))
-            elif uid not in list_a and uid in list_b:  # new item in b
-                prefetch_items_from_b.append(uid)
-                actions.append(('upload', uid, 'b', 'a'))
-        else:
-            if uid in list_a and uid in list_b:
-                if list_a[uid] != status[uid][0] and list_b[uid] != status[uid][1]:
-                    1/0  # conflict resolution TODO
-                elif list_a[uid] != status[uid][0]:  # item update in a
-                    prefetch_items_from_a.append(uid)
-                    actions.append(('update', uid, 'a', 'b'))
-                elif list_b[uid] != status[uid][1]:  # item update in b
-                    prefetch_items_from_b.append(uid)
-                    actions.append(('update', uid, 'b', 'a'))
-                else:  # completely in sync!
-                    pass
-            elif uid in list_a and uid not in list_b:  # deleted from b
-                actions.append(('delete', uid, 'b', 'a'))
-            elif uid not in list_a and uid in list_b:  # deleted from a
-                actions.append(('delete', uid, 'a', 'b'))
+    storages = {
+        'a': [storage_a, items_a, list_a],
+        'b': [storage_b, items_b, list_b]
+    }
+    
+    def get_actions():
+        already_handled = set()
+        prefetch_from_a = []
+        prefetch_from_b = []
+        actions = []  # list(tuple(action, uid, source, dest))
+        for uid in itertools.chain(list_a, list_b, status):
+            if uid in already_handled:
+                continue
+            already_handled.add(uid)
+            if uid not in status:
+                if uid in list_a and uid in list_b:  # missing status
+                    # TODO: might need some kind of diffing too?
+                    status[uid] = (list_a[uid], list_b[uid])  
+                elif uid in list_a and uid not in list_b:  # new item in a
+                    prefetch_from_a.append(uid)
+                    actions.append(('upload', uid, 'a', 'b'))
+                elif uid not in list_a and uid in list_b:  # new item in b
+                    prefetch_from_b.append(uid)
+                    actions.append(('upload', uid, 'b', 'a'))
+            else:
+                if uid in list_a and uid in list_b:
+                    if list_a[uid] != status[uid][0] and list_b[uid] != status[uid][1]:
+                        1/0  # conflict resolution TODO
+                    elif list_a[uid] != status[uid][0]:  # item update in a
+                        prefetch_from_a.append(uid)
+                        actions.append(('update', uid, 'a', 'b'))
+                    elif list_b[uid] != status[uid][1]:  # item update in b
+                        prefetch_from_b.append(uid)
+                        actions.append(('update', uid, 'b', 'a'))
+                    else:  # completely in sync!
+                        pass
+                elif uid in list_a and uid not in list_b:  # deleted from b
+                    actions.append(('delete', uid, 'b', 'a'))
+                elif uid not in list_a and uid in list_b:  # deleted from a
+                    actions.append(('delete', uid, 'a', 'b'))
+                elif uid not in list_a and uid not in list_b:  # deleted from a and b
+                    del status[uid]
+        return actions, prefetch_from_a, prefetch_from_b
+    actions, prefetch_from_a, prefetch_from_b = get_actions()
 
-    items_a = {}
-    items_b = {}
-    for item, uid, etag in storage_a.get_items(prefetch_items_from_a):
+    for item, uid, etag in storage_a.get_multi(prefetch_from_a):
         items_a[uid] = (item, etag)
-    for item, uid, etag in storage_b.get_items(prefetch_items_from_b):
+    for item, uid, etag in storage_b.get_multi(prefetch_from_b):
         items_b[uid] = (item, etag)
 
     for action, uid, source, dest in actions:
-        source_storage = storage_a if source == 'a' else storage_b
-        dest_storage = storage_a if dest == 'a' else storage_b
-        source_items = items_a if source == 'a' else items_b
+        source_storage, source_items, source_list = storages[source]
+        dest_storage, dest_items, dest_list = storages[dest]
+
         if action in ('upload', 'update'):
             item, source_etag = source_items[uid]
             if action == 'upload':
                 dest_etag = dest_storage.upload(item)
             else:
-                dest_etag = dest_storage.update(item, etag)
+                dest_etag = dest_storage.update(item, dest_list[uid])
             status[uid] = (source_etag, dest_etag) if source == 'a' else (dest_etag, source_etag)
         elif action == 'delete':
-            dest_storage.delete(uid)
+            dest_storage.delete(uid, dest_list[uid])
             del status[uid]
