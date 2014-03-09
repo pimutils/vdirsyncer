75eb5a06d75f7007a0ff6ac2b586b16bca22ad55
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-03-09 22:04:54 +0100

    Fix missing sqlalchemy dependency

--- install-deps.sh
+++ install-deps.sh
@@ -1,15 +1,19 @@
 pip install --use-mirrors .
 pip install --use-mirrors -r requirements.txt
 [[ -z "$DAV_SERVER" ]] && DAV_SERVER=radicale
-case "$DAV_SERVER" in
-    "radicale")
-        pip install --use-mirrors radicale
-        ;;
-    "radicale-git")
-        pip install git+https://github.com/Kozea/Radicale.git
-        ;;
-    *)
-        echo "$DAV_SERVER is not known."
-        exit 1
-        ;;
-esac
+
+radicale_deps() {
+    if [[ "$RADICALE_STORAGE" == "database" ]]; then
+        pip install --use-mirrors sqlalchemy
+    fi
+}
+
+davserver_radicale() {
+    pip install --use-mirrors radicale
+}
+
+davserver_radicale_git() {
+    pip install git+https://github.com/Kozea/Radicale.git
+}
+
+davserver_$DAV_SERVER
