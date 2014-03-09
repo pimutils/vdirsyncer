221968b787fdccb6e87bb830976c9c654258afd1
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-03-09 22:12:37 +0100

    Goddamnit

--- install-deps.sh
+++ install-deps.sh
@@ -1,19 +1,23 @@
+#!/bin/sh
 pip install --use-mirrors .
 pip install --use-mirrors -r requirements.txt
 [[ -z "$DAV_SERVER" ]] && DAV_SERVER=radicale
-
-radicale_deps() {
-    if [[ "$RADICALE_STORAGE" == "database" ]]; then
-        pip install --use-mirrors sqlalchemy
-    fi
-}
+[[ -z "$RADICALE_STORAGE" ]] && RADICALE_STORAGE=filesystem
 
 davserver_radicale() {
     pip install --use-mirrors radicale
+    radicale_deps
 }
 
 davserver_radicale_git() {
     pip install git+https://github.com/Kozea/Radicale.git
+    radicale_deps
 }
+
+radicale_deps() { radicale_storage_$RADICALE_STORAGE; }
+
+radicale_storage_database() { pip install --use-mirrors sqlalchemy pysqlite; }
+radicale_storage_filesystem() { true; }
+
 
 davserver_$DAV_SERVER
