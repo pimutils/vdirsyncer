ff688ca2b9278a3d2447aa93e27878f9e1a8b4ce
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-03-19 16:03:57 +0100

    Make assertions a bit narrower again...
    and use ownCloud/SabreDAV normalized form for item template instead

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,12 @@
 def normalize_item(item):
+    # - X-RADICALE-NAME is used by radicale, because hrefs don't really exist
+    #   in their filesystem backend
+    # - PRODID is changed by radicale for some reason after upload, but nobody
+    #   cares about that anyway
     rv = set()
     for line in item.raw.splitlines():
         line = line.strip()
         line = line.strip().split(u':', 1)
-        line[0] = line[0].split(';')[0]
         if line[0] in ('X-RADICALE-NAME', 'PRODID', 'REV'):
             continue
         rv.add(u':'.join(line))
