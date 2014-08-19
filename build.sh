eb0833e1800b8b1dd4f83346a4eb6de629729a94
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-08-19 22:56:40 +0200

    Make CI scripts more readable.

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 set -e
+
+[ -n "$BUILD" ] || BUILD=tests
+[ -n "$DAV_SERVER" ] || DAV_SERVER=radicale
+[ -n "$REQUIREMENTS" ] || REQUIREMENTS=release
+[ -n "$PIP_INSTALL" ] || PIP_INSTALL="pip install"
+[ -n "$TESTSERVER_BASE"] || TESTSERVER_BASE=./tests/storage/dav/servers/
+
+
+_optimize_pip() {
+    # Optimize pip for packages with many C extensions. Comes with its own
+    # cost, e.g. not worth it when running a style checker.
+
+    if [ "$TRAVIS" = "true" ]; then
+        export CFLAGS=-O0  # speed up builds of packages which don't have wheels
+        $PIP_INSTALL --upgrade pip
+        $PIP_INSTALL wheel
+        PIP_INSTALL="pip install --use-wheel --find-links=http://travis-wheels.unterwaditzer.net/wheels/"
+    fi
+}
+
+
 _davserver() {
     # Maybe tmpfs is mounted on /tmp/, can't harm anyway.
     if [ ! -d $TESTSERVER_BASE$1/ ]; then
@@ -11,24 +32,15 @@ _davserver() {
     (cd $TESTSERVER_BASE$1 && sh install.sh)
 }
 
-install_build_tests() {
-    $PIP_INSTALL \
-        coverage \
-        pytest \
-        pytest-xprocess
+command__install_tests() {
+    $PIP_INSTALL pytest pytest-xprocess
+    _optimize_pip
     _davserver $DAV_SERVER
-    if [ "$TRAVIS" = "true" ]; then
-        export CFLAGS=-O0  # speed up builds of packages which don't have wheels
-        $PIP_INSTALL --upgrade pip
-        $PIP_INSTALL wheel
-        PIP_INSTALL="pip install --use-wheel --find-links=http://travis-wheels.unterwaditzer.net/wheels/"
-        $PIP_INSTALL coveralls
-    fi
-
+    [ "$TRAVIS" != "true" ] || $PIP_INSTALL coverage coveralls
     $PIP_INSTALL --editable .
 }
 
-run_build_tests() {
+command__tests() {
     if [ "$TRAVIS" = "true" ]; then
         coverage run --source=vdirsyncer/,tests/ --module pytest
         coveralls
@@ -37,54 +49,30 @@ run_build_tests() {
     fi
 }
 
-install_build_style() {
+command__install_style() {
     $PIP_INSTALL flake8
 }
 
-run_build_style() {
+command__style() {
     flake8 vdirsyncer tests
     ! git grep -il syncroniz $(ls | grep -v 'build.sh')
 }
 
-install_build_docs() {
+command__install_docs() {
     $PIP_INSTALL sphinx sphinx_rtd_theme
     $PIP_INSTALL -e .
 }
 
-run_build_docs() {
+command__docs() {
     cd docs
     make html
 }
 
 
-[ -n "$BUILD" ] || BUILD=tests
-[ -n "$DAV_SERVER" ] || DAV_SERVER=radicale
-[ -n "$REQUIREMENTS" ] || REQUIREMENTS=release
 COMMAND="$1"
 if [ -z "$COMMAND" ]; then
-    echo "Usage:"
-    echo "build.sh run      # run build"
-    echo "build.sh install  # install dependencies"
-    echo
-    echo "Environment variable combinations:"
-    echo "BUILD=tests  # install and run tests"
-    echo "             # (using Radicale, see .travis.yml for more)"
-    echo "BUILD=style  # install and run stylechecker (flake8)"
-    echo "BUILD=docs   # install sphinx and build HTML docs"
+    echo "Usage: build.sh command"
     exit 1
 fi
 
-TESTSERVER_BASE=./tests/storage/dav/servers/
-
-install_builds() {
-    echo "Installing for $BUILD"
-    PIP_INSTALL="pip install"
-    install_build_$BUILD
-}
-
-run_builds() {
-    echo "Running $BUILD"
-    run_build_$BUILD
-}
-
-${COMMAND}_builds
+command__${COMMAND}
