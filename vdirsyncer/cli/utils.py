943c97028630cf6aaffa67a90abc67e6aa65f943
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-02-15 21:29:58 +0100

    Don't rediscover if conflict_resolution changed

--- vdirsyncer/cli/utils.py
+++ vdirsyncer/cli/utils.py
@@ -114,7 +114,10 @@ def get_status_name(pair, collection):
 
 def _get_collections_cache_key(pair_options, config_a, config_b):
     m = hashlib.sha256()
-    j = json.dumps([pair_options, config_a, config_b], sort_keys=True)
+    j = json.dumps([
+        pair_options.get('collections', None),
+        config_a, config_b
+    ], sort_keys=True)
     m.update(j.encode('utf-8'))
     return m.hexdigest()
 
