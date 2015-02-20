2a16f39c808a677ffec03c367ae4688cd01a1ebb
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-02-20 13:53:25 +0100

    Version 0.4.3

--- vdirsyncer/__init__.py
+++ vdirsyncer/__init__.py
@@ -2,7 +2,7 @@
 '''
 vdirsyncer is a synchronization tool for vdir. See the README for more details.
 '''
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 
 PROJECT_HOME = 'https://github.com/untitaker/vdirsyncer'
 DOCS_HOME = 'https://vdirsyncer.readthedocs.org/en/latest'
