3eee5a55a1c87ff849814b643d8c7313b9e57490
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-03-04 12:38:17 +0100

    Hypothesis: Use more idiomatic filter instead of return

--- tests/cli/test_repair.py
+++ tests/cli/test_repair.py
@@ -40,17 +40,15 @@ def test_repair_uids(uid):
     assert uid1 != uid2
 
 
-@given(uid=uid_strategy)
+@given(uid=uid_strategy.filter(lambda x: not href_safe(x)))
 @settings(perform_health_check=False)  # Using the random module for UIDs
 def test_repair_unsafe_uids(uid):
-    if href_safe(uid):
-        return
-
     s = MemoryStorage()
     item = Item(u'BEGIN:VCARD\nUID:{}\nEND:VCARD'.format(uid))
     print(repr(item.raw))
     href, etag = s.upload(item)
     assert s.get(href)[0].uid == uid
+    assert not href_safe(uid)
 
     repair_storage(s)
 
