7a5241101e811a02ab14f4f79fb2f35b4ee8590c
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-04-07 19:52:25 +0200

    Don't test development Radicale

--- tests/storage/servers/radicale/install.sh
+++ tests/storage/servers/radicale/install.sh
@@ -6,15 +6,7 @@ if [ -z "$RADICALE_BACKEND" ]; then
     false
 fi
 
-if [ "$REQUIREMENTS" = "release" ] || [ "$REQUIREMENTS" = "minimal" ]; then
-    radicale_pkg="radicale"
-elif [ "$REQUIREMENTS" = "devel" ]; then
-    radicale_pkg="git+https://github.com/Kozea/Radicale.git"
-else
-    echo "Invalid requirements envvar"
-    false
-fi
-pip install wsgi_intercept $radicale_pkg
+pip install wsgi_intercept radicale
 
 if [ "$RADICALE_BACKEND" = "database" ]; then
     pip install sqlalchemy
