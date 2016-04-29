5749986b54c104477b2457e50bbb922297d9aaa6
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-04-29 20:23:02 +0200

    Don't require coverage for internal assertions

--- vdirsyncer/exceptions.py
+++ vdirsyncer/exceptions.py
@@ -10,7 +10,7 @@ class Error(Exception):
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
-            if getattr(self, key, object()) is not None:
+            if getattr(self, key, object()) is not None:  # pragma: no cover
                 raise TypeError('Invalid argument: {}'.format(key))
             setattr(self, key, value)
 
