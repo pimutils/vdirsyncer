cf225cc03808ed55ba40de466ddfd13b955590e4
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-04-29 20:17:01 +0200

    Add comment

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -439,6 +439,8 @@ class SyncMachine(RuleBasedStateMachine):
         old_items_b = self._get_items(b)
 
         try:
+            # If one storage is read-only, double-sync because changes don't
+            # get reverted immediately.
             for _ in range(2 if a.read_only or b.read_only else 1):
                 sync(a, b, status,
                      force_delete=force_delete,
