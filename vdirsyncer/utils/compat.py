03c0ef60bdda126e5108e51720e40e202bc9b15c
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-09-07 20:39:46 +0200

    Remove to_bytes

--- vdirsyncer/utils/compat.py
+++ vdirsyncer/utils/compat.py
@@ -5,9 +5,3 @@ def to_unicode(x, encoding='ascii'):
     if not isinstance(x, str):
         x = x.decode(encoding)
     return x
-
-
-def to_bytes(x, encoding='ascii'):
-    if not isinstance(x, bytes):
-        x = x.encode(encoding)
-    return x
