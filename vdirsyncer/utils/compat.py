cb4362bb8ee231184a8b8684f5b296f17e9ee3b0
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-09-07 20:37:30 +0200

    Remove unused PY2 variable

--- vdirsyncer/utils/compat.py
+++ vdirsyncer/utils/compat.py
@@ -4,14 +4,6 @@ import sys
 
 from inspect import getfullargspec as getargspec_ish  # noqa
 
-PY2 = sys.version_info[0] == 2
-
-if sys.version_info < (3, 3) and \
-   sys.version_info[:2] != (2, 7):  # pragma: no cover
-    raise RuntimeError(
-        'vdirsyncer only works on Python versions 2.7.x and 3.3+'
-    )
-
 
 def to_unicode(x, encoding='ascii'):
     if not isinstance(x, str):
