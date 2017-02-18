10490a12f13f03495e0945eb9d45d7aed9ab0a6c
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-02-18 15:45:06 +0100

    Unconditionally disable HealthCheck.too_slow

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,10 +26,12 @@ except ImportError:
 else:
     del pytest_benchmark
 
+
+settings.suppress_health_check = [HealthCheck.too_slow]
+
 settings.register_profile("ci", settings(
     max_examples=1000,
     verbosity=Verbosity.verbose,
-    suppress_health_check=[HealthCheck.too_slow]
 ))
 settings.register_profile("deterministic", settings(
     derandomize=True,
