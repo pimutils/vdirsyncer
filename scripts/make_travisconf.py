56b0b48d90cb06cdabef628cbc27195e811541fc
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-03-14 16:05:30 +0100

    Skip remote tests on PRs

--- scripts/make_travisconf.py
+++ scripts/make_travisconf.py
@@ -26,15 +26,20 @@ make -e install-dev;
 make -e install-$BUILD;
 """]
 
-cfg['script'] = ["make -e $BUILD"]
-cfg['after_script'] = ["make -e after-$BUILD"]
+script = """
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] || [ "$BUILD_PRS" != "false" ];
+then {};
+fi""".format
+
+cfg['script'] = [script("make -e $BUILD")]
+cfg['after_script'] = [script("make -e after-$BUILD")]
 
 matrix = []
 cfg['matrix'] = {'include': matrix}
 
 matrix.append({
     'python': latest_python,
-    'env': 'BUILD=style'
+    'env': 'BUILD=style BUILD_PRS=true'
 })
 
 
@@ -52,20 +57,24 @@ for python, requirements in itertools.product(python_versions,
         (("REMOTESTORAGE", x) for x in rs_servers),
         (("DAV", x) for x in dav_servers)
     ):
+
+        build_prs = server not in ("fastmail", "davical", "icloud")
         matrix.append({
             'python': python,
             'env': ("BUILD=test "
                     "{server_type}_SERVER={server} "
                     "REQUIREMENTS={requirements} "
+                    "BUILD_PRS={build_prs} "
                     .format(server_type=server_type,
                             server=server,
-                            requirements=requirements))
+                            requirements=requirements,
+                            build_prs=build_prs and "true" or "false"))
         })
 
 matrix.append({
     'language': 'generic',
     'os': 'osx',
-    'env': 'BUILD=test'
+    'env': 'BUILD=test BUILD_PRS=true'
 })
 
 json.dump(cfg, sys.stdout, sort_keys=True, indent=2)
