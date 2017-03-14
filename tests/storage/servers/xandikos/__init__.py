65c50a8d5238174030fc1f0bf42f35228fbc5ee6
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-03-14 10:54:51 +0100

    Add xandikos to CI

---
+++ tests/storage/servers/xandikos/__init__.py
@@ -0,0 +1,31 @@
+from xandikos.web import XandikosApp, XandikosBackend
+
+import wsgi_intercept
+import wsgi_intercept.requests_intercept
+
+class ServerMixin(object):
+    @pytest.fixture
+    def get_storage_args(self, request, tmpdir, slow_create_collection):
+        backend = XandikosBackend(path=str(tmpdir.mkdir('xandikos')))
+        cup = '/user/'
+        backend._mark_as_pricipal(cup)
+        app = XandikosApp(backend, cup)
+
+        wsgi_intercept.requests_intercept.install()
+        wsgi_intercept.add_wsgi_intercept('127.0.0.1', 8080, lambda: app)
+
+        def teardown():
+            wsgi_intercept.remove_wsgi_intercept('127.0.0.1', 8080)
+            wsgi_intercept.requests_intercept.uninstall()
+        request.addfinalizer(teardown)
+
+        def inner(collection='test'):
+            url = 'http://127.0.0.1:8080/'
+            args = {'url': url, 'username': 'bob', 'password': 'bob',
+                    'collection': collection}
+
+            if collection is not None:
+                args = slow_create_collection(self.storage_class, args,
+                                              collection)
+            return args
+        return inner
---
+++ tests/storage/servers/xandikos/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pip install wsgi_intercept xandikos
