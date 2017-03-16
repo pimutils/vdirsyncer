d02168acff0d314f99d90260c8f943c03cabd2d9
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-03-16 14:24:39 +0100

    Refactor xandikos setup

--- tests/storage/servers/xandikos/__init__.py
+++ tests/storage/servers/xandikos/__init__.py
@@ -1,6 +1,6 @@
 import pytest
 
-from xandikos.web import XandikosApp, XandikosBackend
+from xandikos.web import XandikosApp, XandikosBackend, WellknownRedirector
 
 import wsgi_intercept
 import wsgi_intercept.requests_intercept
@@ -9,10 +9,13 @@ import wsgi_intercept.requests_intercept
 class ServerMixin(object):
     @pytest.fixture
     def get_storage_args(self, request, tmpdir, slow_create_collection):
-        backend = XandikosBackend(path=str(tmpdir.mkdir('xandikos')))
+        tmpdir.mkdir('xandikos')
+        backend = XandikosBackend(path=str(tmpdir))
         cup = '/user/'
-        backend._mark_as_principal(cup)
+        backend.create_principal(cup, create_defaults=True)
         app = XandikosApp(backend, cup)
+
+        app = WellknownRedirector(app, '/')
 
         wsgi_intercept.requests_intercept.install()
         wsgi_intercept.add_wsgi_intercept('127.0.0.1', 8080, lambda: app)
@@ -23,11 +26,12 @@ class ServerMixin(object):
         request.addfinalizer(teardown)
 
         def inner(collection='test'):
-            url = 'http://127.0.0.1:8080/user/'
+            url = 'http://127.0.0.1:8080/'
             args = {'url': url}
 
             if collection is not None:
-                args = slow_create_collection(self.storage_class, args,
-                                              collection)
+                args = self.storage_class.create_collection(
+                    **args, collection=collection
+                )
             return args
         return inner
