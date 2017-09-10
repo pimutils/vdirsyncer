b4d206edbf800525aad9726b455e4263c45dd65e
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-09-10 19:54:25 +0200

    Remove invalid ascii chars from xml, fix #626

--- tests/storage/dav/test_main.py
+++ tests/storage/dav/test_main.py
@@ -1,8 +1,14 @@
-from vdirsyncer.storage.dav import _merge_xml, _parse_xml
+import pytest
+
+from hypothesis import given
+import hypothesis.strategies as st
+
+from vdirsyncer.storage.dav import _BAD_XML_CHARS, _merge_xml, _parse_xml, \
+    _clean_body
 
 
 def test_xml_utilities():
-    x = _parse_xml('''<?xml version="1.0" encoding="UTF-8" ?>
+    x = _parse_xml(b'''<?xml version="1.0" encoding="UTF-8" ?>
         <D:multistatus xmlns:D="DAV:">
             <D:response>
                 <D:propstat>
@@ -26,3 +32,14 @@ def test_xml_utilities():
     props = _merge_xml(response.findall('{DAV:}propstat/{DAV:}prop'))
     assert props.find('{DAV:}resourcetype/{DAV:}collection') is not None
     assert props.find('{DAV:}getcontenttype') is not None
+
+
+@pytest.mark.parametrize('char', range(32))
+def test_xml_specialchars(char):
+    char = chr(char).encode('ascii')
+    x = _parse_xml(b'<?xml version="1.0" encoding="UTF-8" ?>'
+                   b'<foo>ye%ss\r\n'
+                   b'hello</foo>' % char)
+
+    if char in _BAD_XML_CHARS:
+        assert x.text == 'yes\nhello'
