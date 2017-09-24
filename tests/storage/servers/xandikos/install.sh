88969ae5e68e2feafdd633918ff8bd48d1323c99
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-09-24 12:02:24 +0200

    Fix xandikos git url

--- tests/storage/servers/xandikos/install.sh
+++ tests/storage/servers/xandikos/install.sh
@@ -6,7 +6,7 @@ pip install wsgi_intercept
 if [ "$REQUIREMENTS" = "release" ] || [ "$REQUIREMENTS" = "minimal" ]; then
     pip install -U xandikos
 elif [ "$REQUIREMENTS" = "devel" ]; then
-    pip install -U git+https://jelmer.uk/code/xandikos/
+    pip install -U git+https://github.com/jelmer/xandikos
 else
     echo "Invalid REQUIREMENTS value"
     false
