de1903cc4b375920d099cdda02eaaeb1e9232ad6
Amanda Hickman <amanda+github@velociraptor.info>
2017-12-31 17:18:22 -0800

    Fixed spelling of "occurred"

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -2,7 +2,7 @@ from vdirsyncer import exceptions
 
 
 def test_user_error_problems():
-    e = exceptions.UserError('A few problems occured', problems=[
+    e = exceptions.UserError('A few problems occurred', problems=[
         'Problem one',
         'Problem two',
         'Problem three'
