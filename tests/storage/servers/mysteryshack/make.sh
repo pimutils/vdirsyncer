45d76c889c57c27b4894d6c46e38a2e36e7cff26
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-01-21 20:51:14 +0100

    Remove remotestorage leftovers

--- tests/storage/servers/mysteryshack/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# pytest-xprocess doesn't allow us to CD into a particular directory before
-# launching a command, so we do it here.
-cd "$(dirname "$0")"
-. ./variables.sh
-cd mysteryshack
-exec make "$@"
