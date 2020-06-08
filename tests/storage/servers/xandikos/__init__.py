0f3b2e74c0b1930e8526d369e418f5d2052bf688
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-06-08 20:26:19 +0200

    Test xandikos running it in docker
    The main advantage here is that its dependencies are TOTALLY separate
    from vdirsyncer's, keeping the runtime environment for vdirsyncer
    cleaner.
    
    It also makes testing locally not only possible, but fast and pleasant.

--- tests/storage/servers/xandikos/__init__.py
+++ tests/storage/servers/xandikos/__init__.py
@@ -1,35 +1,15 @@
 import pytest
 
-from xandikos.web import XandikosApp, XandikosBackend, WellknownRedirector
-
-import wsgi_intercept
-import wsgi_intercept.requests_intercept
-
 
 class ServerMixin:
     @pytest.fixture
     def get_storage_args(self, request, tmpdir, slow_create_collection):
-        tmpdir.mkdir('xandikos')
-        backend = XandikosBackend(path=str(tmpdir))
-        cup = '/user/'
-        backend.create_principal(cup, create_defaults=True)
-        app = XandikosApp(backend, cup)
-
-        app = WellknownRedirector(app, '/')
-
-        wsgi_intercept.requests_intercept.install()
-        wsgi_intercept.add_wsgi_intercept('127.0.0.1', 8080, lambda: app)
-
-        def teardown():
-            wsgi_intercept.remove_wsgi_intercept('127.0.0.1', 8080)
-            wsgi_intercept.requests_intercept.uninstall()
-        request.addfinalizer(teardown)
-
-        def inner(collection='test'):
-            url = 'http://127.0.0.1:8080/'
-            args = {'url': url, 'collection': collection}
+        def inner(collection="test"):
+            url = "http://127.0.0.1:8000/"
+            args = {"url": url}
 
             if collection is not None:
-                args = self.storage_class.create_collection(**args)
+                args = slow_create_collection(self.storage_class, args, collection)
             return args
+
         return inner
