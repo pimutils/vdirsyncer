7c04289ed415a5a64ba558c9fb05de7275bd519a
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-06-08 13:51:15 +0200

    Pin xandikos to 0.1.0

--- tests/storage/servers/xandikos/install.sh
+++ tests/storage/servers/xandikos/install.sh
@@ -4,7 +4,8 @@ set -e
 pip install wsgi_intercept
 
 if [ "$REQUIREMENTS" = "release" ] || [ "$REQUIREMENTS" = "minimal" ]; then
-    pip install -U xandikos
+    # XXX: This is the last version to support Python 3.5
+    pip install -U "xandikos==0.1.0"
 elif [ "$REQUIREMENTS" = "devel" ]; then
     pip install -U git+https://github.com/jelmer/xandikos
 else
