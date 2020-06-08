9505430b83958662a1b248736fa778060d039218
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-06-08 16:27:50 +0200

    Pin an older version of Xandikos
    This is the one used back when master was last green, so let's start
    with this one and gradually move up.

--- tests/storage/servers/xandikos/install.sh
+++ tests/storage/servers/xandikos/install.sh
@@ -5,7 +5,7 @@ pip install wsgi_intercept
 
 if [ "$REQUIREMENTS" = "release" ] || [ "$REQUIREMENTS" = "minimal" ]; then
     # XXX: This is the last version to support Python 3.5
-    pip install -U "xandikos==0.1.0"
+    pip install -U "xandikos==0.0.11"
 elif [ "$REQUIREMENTS" = "devel" ]; then
     pip install -U git+https://github.com/jelmer/xandikos
 else
