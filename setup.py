961203e86515a7601b0aafcd232d5a740a12875a
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-11-02 18:07:13 +0100

    Require aiohttp >= 3.8.0
    See https://github.com/pimutils/vdirsyncer/issues/916#issuecomment-957671206
    See https://github.com/aio-libs/aiohttp/issues/5156

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
     "requests_toolbelt >=0.4.0",
     # https://github.com/untitaker/python-atomicwrites/commit/4d12f23227b6a944ab1d99c507a69fdbc7c9ed6d  # noqa
     "atomicwrites>=0.1.7",
-    "aiohttp>=3.7.1,<4.0.0",
+    "aiohttp>=3.8.0,<4.0.0",
     "aiostream>=0.4.3,<0.5.0",
 ]
 
